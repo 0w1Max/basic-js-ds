ffac987fc3ac280eaf3d11479ca30d88a0eadd92
Ow1Max <0w1.go.0w1@gmail.com>
2023-04-18 02:56:33 +0300

    feat: Completed the task remove-from-list

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -22,9 +22,26 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function removeKFromList(l, k) {
+  if (!l || (l.value === k && !l.next)) {
+    return null;
+  }
+
+  while (l && l.value === k) {
+    l = l.next
+  }
+
+  let curr = l;
+
+  while (curr && curr.next) {
+    if (curr.next.value === k) {
+      curr.next = curr.next.next;
+    } else {
+      curr = curr.next;
+    }
+  }
+
+  return l;
 }
 
 module.exports = {
